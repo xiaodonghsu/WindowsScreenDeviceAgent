ed9db6812efd610797cfbeb75e210be190a008fe
FMStereo <xiaodong_hsu@sohu.com>
2026-04-21 18:02:54 +0800

    断线后出现无法接收RPC的问题：重连没有重新订阅主题！

--- iot/thingsboardclient.py
+++ iot/thingsboardclient.py
@@ -1,20 +1,39 @@
 import json
 import paho.mqtt.client as mqtt
 import time
+import logging
+
+from common.config import get_device_name
+logger = logging.getLogger(get_device_name())
 
 class ThingsBoardClient:
     def __init__(self, host, port, token):
         self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
         self.client.username_pw_set(token)
         self.client.connect(host, port, 60)
         self.attributes = {}
         self._req_id = 1
         self._pending_req: Dict[int, float] = {}  # req_id -> time
 
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            logger.info("连接ThingsBoard成功, 订阅主题...")
+            self.client.subscribe("v1/devices/me/rpc/request/+")
+            self.client.subscribe("v1/devices/me/attributes")
+        else:
+            logger.error(f"连接失败，代码: {rc}")
+
+    def on_disconnect(self, client, userdata, rc):
+        logger.info(f"ThingsBoard连接断开, 代码: {rc}")
+        if rc != 0:
+            logger.error("异常断开，自动重连机制已触发...")
+            # 库的自动重连已配置，此处也可添加自定义日志或状态更新
+        # 注意：如果clean_session=False，重连后会自动恢复会话，无需手动重订阅
+
     def start(self, on_message):
         self.client.on_message = on_message
-        self.client.subscribe("v1/devices/me/rpc/request/+")
-        self.client.subscribe("v1/devices/me/attributes")
         self.client.loop_start()
 
     def send_telemetry(self, data: dict):
